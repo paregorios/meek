562aee5a8f7786f499deac5b9be6f711854e8607
Tom Elliott <tom.elliott@nyu.edu>
2021-10-12 15:44:30 -0500

    help and usage

--- meek/interpreter.py
+++ meek/interpreter.py
@@ -4,6 +4,7 @@
 Command interpreter for meek
 """
 
+from inspect import getdoc
 import logging
 from meek.manager import Manager
 import readline
@@ -17,6 +18,11 @@ class Interpreter:
     def __init__(self):
         self.manager = Manager()
         self.verbs = [a[6:] for a in dir(self) if a.startswith('_verb_')]
+        self.aliases = {
+            'q': 'quit',
+            'h': 'help',
+            '?': 'help'
+        }
 
     def parse(self, parts):
         verb = parts[0]
@@ -25,7 +31,10 @@ class Interpreter:
         except IndexError:
             objects = []
         if verb not in self.verbs:
-            return f'Unrecognized verb "{verb}"'
+            try:
+                verb = self.aliases[verb]
+            except KeyError:
+                return f'Unrecognized verb "{verb}"'
         args, kwargs = self._objectify(objects)
         return getattr(self, f'_verb_{verb}')(args, **kwargs)
 
@@ -49,9 +58,49 @@ class Interpreter:
                 args.append(o)
         return (args, kwargs)
 
+    def _usage(self, verb):
+        """Print usage for indicated command"""
+        usage = getdoc(getattr(self, f'_verb_{verb}')).splitlines()[1:]
+        return '\n'.join(usage)
+
+    def _verb_help(self, args, **kwargs):
+        """
+        Get help with available commands.
+            > help (lists all available command verbs)
+            > help {verb} (prints usage for the indicated verb)
+        """
+        if args:
+            return self._usage(' '.join(args))
+        else:
+            entries = [
+                (
+                    k.split('_')[-1],
+                    getdoc(getattr(self, f'_verb_{k}')).splitlines()[0]
+                ) for k in self.verbs]
+            entries.sort(key=lambda x: x[0])
+            longest = max([len(e[0]) for e in entries])
+            entries = [f'{e[0]}:'.rjust(
+                longest+1) + f' {e[1]}' for e in entries]
+            return '\n'.join(entries)
+
     def _verb_new(self, args, **kwargs):
+        """
+        Create a new activity.
+            > new
+              creates a new, empty activity
+            > new Take a nap
+              creates a new activity with title "Take a nap"
+            > new Take a nap due=today
+            > new Take a nap due:today
+            > new Take a nap due:2021-07-03
+            > new Take a nap tags=personal,home,health
+        """
         return f'New! "{args}" "{kwargs}"'
 
-    def _verb_quit(self, objects):
-        """Quit interactive interface."""
+    def _verb_quit(self, args, **kwargs):
+        """
+        Quit interactive interface.
+            > quit
+            WARNING: unsaved data will be lost (use "save" first)
+        """
         exit()
